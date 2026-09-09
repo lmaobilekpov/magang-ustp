5755e5c0e549446274e740df906f474c77dd0b79
lmaobilekpov <rajendraadityawan1@gmail.com>
2026-09-09 08:55:14 +0700

    feat: menyelesaikan masalah gambar

--- ustp_tracking/urls.py
+++ ustp_tracking/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from dokumen import views  # Import file views yang baru aja lu bikin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.lacak_dokumen, name='lacak_dokumen'),
+]
 
 urlpatterns = [
     # Ini pintu belakang buat lu dan resepsionis
@@ -9,3 +16,6 @@ urlpatterns = [
     # Ini pintu depan (Portal Karyawan), roket Django resmi digusur!
     path('', views.lacak_dokumen, name='lacak_dokumen'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
