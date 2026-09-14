5be0d2af4cd80137954b744475b9591655311655
lmaobilekpov <rajendraadityawan1@gmail.com>
2026-09-14 15:59:43 +0700

    nganu

--- dokumen/models.py
+++ dokumen/models.py
@@ -4,9 +4,20 @@ from django.core.exceptions import ValidationError
 
 
 class Karyawan(models.Model):
-    kode_karyawan = models.CharField(max_length=50, unique=True, verbose_name="Kode Karyawan")
+    kode_karyawan = models.CharField(
+        max_length=50,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="Kode Karyawan"
+    )
     nama_lengkap = models.CharField(max_length=255, verbose_name="Nama Lengkap")
-    jabatan = models.CharField(max_length=255, verbose_name="Jabatan")
+    jabatan = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name="Jabatan"
+    )
     tanggal_lahir = models.DateField(verbose_name="Tanggal Lahir")
 
     class Meta:
